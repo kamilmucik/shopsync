5e38d6e556f00103a8081f693a6e35464368e8a1
kmucik <k.mucik@e-strix.pl>
2022-03-07 08:14:37 +0100

    FastFix

--- web-app/src/main/java/pl/estrix/shopsync/tool/AbstractRemapIdDeserializer.java
+++ web-app/src/main/java/pl/estrix/shopsync/tool/AbstractRemapIdDeserializer.java
@@ -1,20 +1,14 @@
 package pl.estrix.shopsync.tool;
 
-//import com.alleron.evb.utils.domain.crypt.CryptConf;
-//import com.ailleron.eb.utils.domain.crypt.CryptUtil;
-import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.BeanProperty;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.deser.ContextualDeserializer;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 import pl.estrix.shopsync.commons.annotation.RemapId;
-//import pl.bpsa.service.commons.annotation.Remapd;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Objects;
 
 import static org.apache.commons.lang3.ArrayUtils.isEmpty;
+
 
 public abstract class AbstractRemapIdDeserializer<T> extends StdDeserializer<T> implements ContextualDeserializer {
 
